13c19b45283a3b5f88e71cd3b325b4906d6aecdf
k1m-mj5 <77222844+k1m-mj5@users.noreply.github.com>
2022-02-06 18:26:09 +0900

    binary

---
+++ JAVAEclipse/Doit02/src/binary/BinaryTest.java
@@ -0,0 +1,18 @@
+package binary;
+
+public class BinaryTest {
+	
+	public static void main(String[] args) {
+		
+		int num = 10;
+		int bNum = 0B1010; // 10: Binary number
+		int oNum = 012; // 10: Octal
+		int hNum = 0XA; // 10: Hexadecimal
+		
+		System.out.println(num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(hNum);
+		
+	}
+}
---
+++ JAVAEclipse/Doit02/src/module-info.java
@@ -0,0 +1,2 @@
+module Doit02 {
+}
