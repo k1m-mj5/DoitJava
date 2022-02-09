fc9678f23c7358b4b10b981c45ebc6d9f84aafdf
k1m-mj5 <77222844+k1m-mj5@users.noreply.github.com>
2022-02-09 17:38:34 +0900

    assignment

---
+++ JAVAEclipse/Doit03/src/assignment/OperationEx2.java
@@ -0,0 +1,22 @@
+package assignment;
+
+public class OperationEx2 {
+
+	public static void main(String[] args) {
+		
+		int num = 10;
+		
+		System.out.println(num++); // 10
+		System.out.println(num); // 11
+		System.out.println(++num); // 12
+		
+		System.out.println(3>5); // false
+		
+		int num1 = 10;
+		int num2 = 5;
+		boolean flag = (num1 > num2);
+		System.out.println(flag); // true
+		
+	}
+
+}
---
+++ JAVAEclipse/Doit03/src/assignment/OperationEx3.java
@@ -0,0 +1,24 @@
+package assignment;
+
+public class OperationEx3 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int i = 2;
+		
+		/*
+		boolean value = ( ((num1 = num1 + 10) < 10) && ((i = i+2) > 10) ); 
+		System.out.println(value); // false
+		System.out.println(num1); // 20
+		System.out.println(i); // 2
+		*/
+		
+		boolean value = ( ((num1 = num1 + 10) < 10) || ((i = i+2) > 10) );
+		System.out.println(value); // false
+		System.out.println(num1);  // 20
+		System.out.println(i);  // 4
+		
+	}
+
+}
