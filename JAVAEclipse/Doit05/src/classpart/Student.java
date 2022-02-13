d4dedddcc8cce583ecc016b9561db8b553a674c1
k1m-mj5 <77222844+k1m-mj5@users.noreply.github.com>
2022-02-13 18:18:53 +0900

    new class

--- JAVAEclipse/Doit05/src/classpart/Student.java
+++ JAVAEclipse/Doit05/src/classpart/Student.java
@@ -30,4 +30,20 @@ public class Student {
 	public void setStudentName(String name) {
 		studentName = name;
 	}
+	
+	public static void main(String[] args) {
+		Student student02 = new Student();
+		student02.studentName = "Bob";
+		student02.studentID = 102;
+		student02.address = "London, UK";
+		
+		Student student03 = new Student();
+		student03.studentName = "Carol";
+		student03.studentID = 103;
+		student03.address = "SP, Brazil";
+		
+		student02.showStudentInfor();
+		student03.showStudentInfor();
+		
+	}
 }
